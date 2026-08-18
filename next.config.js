5a40233a286f5f72a07ead62b928706c5f828dd7
T.Anushka <anushka.tatapudi@gmail.com>
2026-08-18 20:45:06 +0530

    Make basePath configurable for deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/portfolio',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH !== undefined ? process.env.NEXT_PUBLIC_BASE_PATH : '/portfolio',
   trailingSlash: true,
   images: {
     unoptimized: true
